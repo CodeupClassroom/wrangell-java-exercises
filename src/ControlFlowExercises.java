c53fe77d04315b6b0826475a818d1b46cebce5ba
Daniel Fryar <daniel@Daniels-MacBook-Pro.local>
2018-09-19 12:31:06 -0500

    4. Bonus

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,13 +7,20 @@ public class ControlFlowExercises {
         while (confirm) {
             System.out.println("Please enter the grade (0-100):");
             int grade = scanner.nextInt();
-            char letterGrade = '*';
+            String letterGrade = "*";
             if (grade >= 0 && grade <= 100) {
-                if (grade >= 88) letterGrade = 'A';
-                else if (grade >= 80) letterGrade = 'B';
-                else if (grade >= 67) letterGrade = 'C';
-                else if (grade >= 60) letterGrade = 'D';
-                else letterGrade = 'F';
+                if (grade >= 98) letterGrade = "A+";
+                else if (grade >= 91) letterGrade = "A";
+                else if (grade >= 88) letterGrade = "A-";
+                else if (grade >= 86) letterGrade = "B+";
+                else if (grade >= 83) letterGrade = "B";
+                else if (grade >= 80) letterGrade = "B-";
+                else if (grade >= 78) letterGrade = "C+";
+                else if (grade >= 75) letterGrade = "C";
+                else if (grade >= 72) letterGrade = "C-";
+                else if (grade >= 67) letterGrade = "D+";
+                else if (grade >= 60) letterGrade = "D";
+                else letterGrade = "F";
             }
             System.out.println("You got a(n) "+letterGrade);
             System.out.println();
