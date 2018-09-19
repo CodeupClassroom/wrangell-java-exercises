baf769c8efc184158d27415db527ca0d9a21beb5
Daniel Fryar <daniel@Daniels-MacBook-Pro.local>
2018-09-19 11:33:42 -0500

    3. Display a table of powers.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i=1;i<=100;i++) {
-            if (i%15==0) System.out.println("FizzBuzz");
-            else if (i%3==0) System.out.println("Fizz");
-            else if (i%5==0) System.out.println("Buzz");
-            else System.out.println(i);
-        }
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What number would you like to go up to?");
+        int userInt = scanner.nextInt();
+        System.out.println("Here is your table!");
+        System.out.println();
+        System.out.println("  number  |  squared  |  cubed  ");
+        System.out.println(" -------- | --------- | -------- ");
+        for (long i = 1; i <= userInt; i++) {
+            System.out.format(" %-9s| %-9s | %-9s%n",
+                    i,
+                    i * i,
+                    i * i * i);
+        }
     }
 }
