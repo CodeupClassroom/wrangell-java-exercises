78c3e73c8f9420b396a3d1da2f1d700fea1e2e3e
Daniel Fryar <daniel@Daniels-MacBook-Pro.local>
2018-09-20 10:46:57 -0500

    pass by reference

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,19 +1,22 @@
 public class Sandbox {
 
-    public static String sayHello(String name, String greeting) {
-        return new String(greeting + ", " +  name + "!");
-    }
-    public static String sayHello(String name) {
-        return sayHello("Hello", name);
-    }
-    public static String sayHello() {
-        return sayHello("Hello", "You");
+    public static String modifyString(String param) {
+        param = "HAHAHAHAH";
+        return param;
     }
 
+    public static void addTwo(int num) {
+        num = num + 2;
+    }
     public static void main(String[] args) {
-        System.out.println(sayHello());
-        System.out.println(sayHello("Wrangell"));
-        System.out.println(sayHello("Wrangell", "Salutations"));
+        String name = "codeup";
+        System.out.println(name);
+        name = modifyString(name);
+        System.out.println(name);
+        int seven = 7;
+        System.out.println(seven);
+        addTwo(seven);
+        System.out.println(seven);
     }
 }
 
