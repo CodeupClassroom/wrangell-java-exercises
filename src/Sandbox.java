80c4f9ad7f85e92bcf18c5ad03a68c8e9a417363
Daniel Fryar <daniel@Daniels-MacBook-Pro.local>
2018-09-26 16:47:07 -0500

    First Exercise

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,34 +1,23 @@
-package yadayada;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import this;
-import that;
 
 public class Sandbox {
-    //static properties
-    static int UNIVERSAL_HEIGHT = 100;
-    //instance properties
-    int height;
-    int width;
-    //constructors
-    public Sandbox() {
-       this.height = UNIVERSAL_HEIGHT;
 
-    }
-    //static methods
-    static void method1(){
-
-    }
-    static void method2(){
-
-    }
-    //instance methods
-
-    //main method last
     public static void main(String[] args) {
-
+//    String[] array = new String[10];
+        HashMap<String,String> classRoster = new HashMap<>();
+        classRoster.put("first", new String("Chris"));
+        classRoster.put("second", "Hello");
+        classRoster.put("fifteenth", "World");
+        classRoster.replace("second", "Goodbye");
+        String hello = new String("Hello");
+        System.out.println(classRoster);
+        classRoster.clear();
+        System.out.println(classRoster);
     }
+
 
 
 }
-
-
---
+++ src/grades/Student.java
@@ -0,0 +1,44 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name;
+    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double gradesTotal = 0;
+        for (int grade:this.grades) gradesTotal+= grade;
+        return gradesTotal / this.grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student jeff = new Student("Jeff");
+        jeff.addGrade(43);
+        jeff.addGrade(86);
+        jeff.addGrade(100000);
+        System.out.println(jeff.getName());
+        System.out.println(jeff.getGradeAverage());
+
+        Student romulus = new Student("romulus");
+        romulus.addGrade(55);
+        romulus.addGrade(65);
+        romulus.addGrade(75 );
+        System.out.println(romulus.getName());
+        System.out.println(romulus.getGradeAverage());
+    }
+}
