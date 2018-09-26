0820beb148c73f643be531dcf34f830452de7922
Daniel Fryar <daniel@Daniels-MacBook-Pro.local>
2018-09-26 16:55:49 -0500

    Second Exercise

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,21 @@
+package grades;
+
+import java.util.HashMap;
+
+public class GradesApplication {
+    public static void main(String[] args) {
+        HashMap<String,Student> students = new HashMap<>();
+
+        Student daniel = new Student("Daniel");
+        daniel.addGrade(42);
+        students.put("danielfryar", daniel);
+
+        Student james = new Student("James");
+        james.addGrade(84);
+        students.put("james.kevin.a.young", james);
+
+        students.put("githubmonster", new Student("student1"));
+        
+    }
+
+}
