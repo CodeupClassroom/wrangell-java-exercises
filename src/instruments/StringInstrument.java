8c82370b0b013e1497da0130d489210d2bac188c
Daniel Fryar <daniel@Daniels-MacBook-Pro.local>
2018-09-26 09:38:01 -0500

    Fixing a bug that I negligently pushed.

--- src/instruments/StringInstrument.java
+++ src/instruments/StringInstrument.java
@@ -1,6 +1,6 @@
 package instruments;
 
-public class StringInstrument extends Instrument implements MuseumArticle {
+public class StringInstrument extends Instrument {
     int numberOfStrings;
 
     public StringInstrument(String maker, int numberOfStrings) {
